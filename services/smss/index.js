1a4dfb73d317a9775df871d591ffc4c9e808957e
Hao Kai <leonkaihao@outlook.com>
2018-12-06 07:56:30 +1100

    dotenv and history view bugs fix

--- services/smss/index.js
+++ services/smss/index.js
@@ -12,7 +12,7 @@ var sms = exports = module.exports = {
             port:443,
             path: "/send-sms.json",
             method: "POST",
-            auth: "ae559db613f7b722e21c7b1108037c46:secret",
+            auth: process.env.SMS_KEY+":"+process.env.SMS_SECRET,
             rejectUnauthorized: false , 
             headers: {
               "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
