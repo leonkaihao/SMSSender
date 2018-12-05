23f389c816435ff39ec3378c64d3b9d66ff77dce
Hao Kai <leonkaihao@outlook.com>
2018-12-05 22:37:42 +1100

    Add SMS send front end

--- public/ng/controllers/smss.js
+++ public/ng/controllers/smss.js
@@ -10,4 +10,39 @@
 angular.module('myApp').controller(
 'SMSsCtrl',['$scope', '$rootScope', 'ApiService',
 function ($scope, $rootScope, ApiService) {
+    $scope.data = {
+        from: "",
+        to: [],
+        message: "",
+        receiver: ""
+    };
+    $scope.history = [];
+    $scope.AddReceiver = function() {
+        if ($scope.data.receiver !== "") {
+            $scope.data.to.push($scope.data.receiver);
+        }
+    };   
+    $scope.DelReceiver = function(index) {
+        $scope.data.to.splice(index, 1);
+    };
+    $scope.Send = function() {        
+		let obj = {
+			params: {
+				token: $rootScope.session.token
+			},
+			data: {
+				sms: {
+                    from: $scope.data.from,
+                    message: $scope.data.message,
+                    to: $scope.data.to.join(",")
+                }
+			}
+        };
+        
+        ApiService.post('/api/smss', obj, function(data) {
+            $scope.history.push(JSON.stringify(data.data));
+        }, function(err) {
+            $scope.history.push(err.message);
+        });
+    };
 }]);
--- routes/smss.js
+++ routes/smss.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var smss = require("../controllers/smss");
 //post an SMS
 router.post('/', function(req, res, next) {
-	smss.send(req.query.token, req.body.data.sms, function(statusCode, result) {
+	smss.sendSMS(req.query.token, req.body.data.sms, function(statusCode, result) {
 		res.status(statusCode).json(result);
 	});
 });
